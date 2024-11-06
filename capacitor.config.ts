bebce698e95720b4139f300b2af0bc1d750a6205
Stefan <bogdan.izdrail@gmail.com>
2024-11-06 08:01:26 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-osint-app',
+  webDir: 'dist'
+};
+
+export default config;
